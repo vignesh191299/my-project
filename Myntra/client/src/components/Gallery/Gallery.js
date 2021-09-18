e0b5b8acd6b367113de86e01f6e98b2fa3ed4373
vignesh191299 <74527448+vignesh191299@users.noreply.github.com>
2021-09-18 14:43:15 +0530

    Add files via upload

---
+++ Myntra/client/src/components/Gallery/Gallery.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import {
+        Container,
+        Row} from 'reactstrap'
+import Name from './Product';
+
+class Home extends Component {
+    constructor() {
+        super();
+        this.state={
+            products:[]
+        };
+}
+componentDidMount() {
+    fetch('http://localhost:5000/products/get')
+      .then(res => res.json())
+      .then(products => this.setState({ products }, () => console.log('images fetched...', products)));
+  }
+ Products(){
+    return this.state.products.map(data => {
+        return ( <div key={data.image} className="col-lg-3 col-sm-12 col-md-6">
+            <Name link={data.link} image={data.image} name={data.name} hint={data.hint} />
+        </div>)
+    });
+ }
+
+  render() {
+      //
+      return (
+          <Container fluid={true}>
+                <h6 style= {{textAlign:"Center",fontSize:"50px", fontStyle:"bold"}}>D  E  A  L  S  O  F  T  H  E  D  A  Y </h6><br/>
+            <Row>
+          {this.Products()}
+          </Row>
+          </Container>
+      );
+  }
+}
+export default Home;
